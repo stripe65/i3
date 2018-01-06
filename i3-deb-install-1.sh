91eec65a005d8b8fd60d5c83559684db8ccb6b1b
stripe65 <donh824@gmail.com>
2018-01-06 20:32:36 +0000

    i3-deb-install-1.sh

---
+++ i3-deb-install-1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt install xorg i3 xfce4-terminal compton geany zim ranger mpv pulseaudio pavucontrol slim nitrogen htop xfce4-power-manager mumble irssi rsync curl ffmpeg 
+
+#sudo apt -t stretch-backports install kodi minetest
+
+#sudo mkdir /media/sdd1
+
+#sudo echo "#mount music dir at boot up" >> /etc/fstab
+
+#sudo echo UUID=9397887e-62f9-40ea-b986-8918226b6f3f /media/sdd1 ext4 defaults 0 0 >> /etc/fstab
+
+sudo reboot
+
+
